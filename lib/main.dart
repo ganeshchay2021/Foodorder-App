0c53707deef3a4fe7b70cdada886443cb135cdf1
ganeshchay2021 <ganeshchay2021@gmail.com>
2024-07-09 13:46:49 +0545

    setup to release app

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   await Future.delayed(
